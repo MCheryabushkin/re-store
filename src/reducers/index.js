5070b78fc962a377b37de2d32451370b12530d33
Maksim Cheryabushkin <ch.maks@list.ru>
2020-07-23 22:37:07 +0300

    Подключение компонента с таблицей заказов к Redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,22 +1,41 @@
 const initialState = {
   books: [],
   loading: true,
-  error: null
+  error: null,
+  cartItems: [{
+    id: 1, 
+    name: "Name 1", 
+    count: 2, 
+    total: 80
+  }, {
+    id: 2, 
+    name: "Name 2", 
+    count: 1, 
+    total: 30
+  }],
+  orderTotal: 220
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "FETCH_BOOKS_REQUEST":
-      return initialState;
+      return {
+        ...state,
+        books: [],
+        loading: true,
+        error: null
+      };
 
     case "FETCH_BOOKS_ERROR":
       return {
+        ...state,
         books: [],
         loading: false,
         error: action.payload
       }
     case "FETCH_BOOKS_SUCCESS":
       return {
+        ...state,
         books: action.payload,
         loading: false,
         error: null
